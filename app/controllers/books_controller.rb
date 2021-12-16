983eb00a94cf7f28c4d26333826711648693b7e2
miyusac <miyusac69@gmail.com>
2021-12-16 12:48:20 +0000

    プロフィール編集機能を追加。自己紹介のカラムはまだ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   end
 
   def index
-      @books = Book.all
+    @books = Book.page(params[:page]).reverse_order
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  
+  resources :users, only: [:show, :edit, :update]
 end
 
