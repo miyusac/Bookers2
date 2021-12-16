282130d7dcf3505490912edd4b212944318c8f7c
miyusac <miyusac69@gmail.com>
2021-12-16 11:58:16 +0000

    編集機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,16 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book[:id])
+  end
 
   def destroy
     @book = Book.find(params[:id])
