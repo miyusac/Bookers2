b65546e14896fe1932ac08d330bf6b4a5137d0bd
miyusac <miyusac69@gmail.com>
2021-12-16 14:47:49 +0000

    バリデーションとエラーメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-has_many :books, dependent: :destroy
-attachment :profile_image
+  has_many :books, dependent: :destroy
+  attachment :profile_image
+
+  validates :name, presence: true, uniqueness: true, length: {　minimum: 2, maximum: 20 }
+  validates :introduction, presence: true, length: { maximum: 50 }
 end
