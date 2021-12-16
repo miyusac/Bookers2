1bebddf3efcc7f50dab6cc9dffa5e11d6b20a177
miyusac <miyusac69@gmail.com>
2021-12-16 13:33:17 +0000

    マイページ自己紹介追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'badd0d55ad35befeaf7c5ae23c56005910ca62a27c8b1a4df10f896f63b1a3b653ab9d95f7cefa5929b470969e0ef93051d069c74799e357f65646ccea9c7e76'
