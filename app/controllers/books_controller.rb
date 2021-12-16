70305fed2d57f17d1c2c801cfb221c1dbbd788c7
miyusac <miyusac69@gmail.com>
2021-12-16 15:51:56 +0000

    サクセスメッセージ機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice]="Book was successfully created."
       redirect_to books_path
     else
       render :new
@@ -27,8 +28,12 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice]="User was successfully updated."
       redirect_to user_path(@user.id)
     else
       render :edit
