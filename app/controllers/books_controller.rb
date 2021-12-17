b18a90774050a0281259db383b701c542f455c95
miyusac <miyusac69@gmail.com>
2021-12-17 06:16:08 +0000

    レイアウト変更とbootstrap導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice]="Book was successfully created."
-      redirect_to books_path
+      redirect_to book_path(@book[:id])
     else
       render :new
     end
