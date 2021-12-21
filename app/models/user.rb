72aadf22ca56b102f2451877b6e1cf429eedfabf
miyusac <miyusac69@gmail.com>
2021-12-21 03:58:05 +0000

    一覧画面でいいねをした後の遷移がまだ

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :profile_image
+  
+
+    
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
