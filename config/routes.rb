003e5decee9d5669c683c9a9b1e8eb8441e59e0c
miyusac <miyusac69@gmail.com>
2021-12-21 11:24:53 +0000

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get "/home/about" =>"homes#about"
   
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
   resources :users, only: [:index, :show, :edit, :update]
